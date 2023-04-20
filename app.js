dcd89383eaae3a9a394cec984fe64597065c4264
S-Chanchon11 <chanchon.splus@gmail.com>
2023-04-20 11:06:27 +0700

    add readme.md

--- app.js
+++ app.js
@@ -5,8 +5,4 @@ console.log("Testing second line")
 console.log("Testing third line")
 console.log("Testing 4 line")
 
-<<<<<<< HEAD
-console.log("Github")
-=======
 console.log("testing from git")
->>>>>>> 1ea3a1c18ae808336594e5b63ce3ac220bc86d4b
